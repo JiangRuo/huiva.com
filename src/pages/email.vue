b82a820491a2546f30c123e739d3ca43c2606a86
JiangRuo <wr123456123@126.com>
2023-09-08 17:26:33 +0800

    9.8号邮箱页面开发

--- src/pages/email.vue
+++ src/pages/email.vue
@@ -54,12 +54,206 @@
               <span>容量</span>
               <span>邮件无限容量</span>
             </p>
+            <p>
+              <span>个人</span>
+              <span>个人网盘5G</span>
+            </p>
+            <p>
+              <span>共享</span>
+              <span>共享网盘15G</span>
+            </p>
+            <p>
+              <span>域名</span>
+              <span>多域名绑定</span>
+            </p>
+            <p>
+              <span>管理</span>
+              <span>多域统一管理</span>
+            </p>
           </div>
           <div class="product_ctn_money">
-
+            <p>
+              <span>0-20</span>
+              /用户
+            </p>
+          </div>
+          <div class="product_ctn_btn">
+            <router-link to="/">
+              免费试用
+            </router-link>
+          </div>
+        </li>
+        <li>
+          <div class="product_ctn_tit">
+            <h3>体验版</h3>
+            <p>描述信息描述信息描述信息描述信息</p>
+          </div>
+          <div class="product_ctn_txt">
+            <p>
+              <span>容量</span>
+              <span>邮件无限容量</span>
+            </p>
+            <p>
+              <span>个人</span>
+              <span>个人网盘5G</span>
+            </p>
+            <p>
+              <span>共享</span>
+              <span>共享网盘15G</span>
+            </p>
+            <p>
+              <span>域名</span>
+              <span>多域名绑定</span>
+            </p>
+            <p>
+              <span>管理</span>
+              <span>多域统一管理</span>
+            </p>
+          </div>
+          <div class="product_ctn_money">
+            <p>
+              <span>0-20</span>
+              /用户
+            </p>
+          </div>
+          <div class="product_ctn_btn">
+            <router-link to="/">
+              免费试用
+            </router-link>
+          </div>
+        </li>
+        <li>
+          <div class="product_ctn_tit">
+            <h3>体验版</h3>
+            <p>描述信息描述信息描述信息描述信息</p>
+          </div>
+          <div class="product_ctn_txt">
+            <p>
+              <span>容量</span>
+              <span>邮件无限容量</span>
+            </p>
+            <p>
+              <span>个人</span>
+              <span>个人网盘5G</span>
+            </p>
+            <p>
+              <span>共享</span>
+              <span>共享网盘15G</span>
+            </p>
+            <p>
+              <span>域名</span>
+              <span>多域名绑定</span>
+            </p>
+            <p>
+              <span>管理</span>
+              <span>多域统一管理</span>
+            </p>
+          </div>
+          <div class="product_ctn_money">
+            <p>
+              <span>0-20</span>
+              /用户
+            </p>
+          </div>
+          <div class="product_ctn_btn">
+            <router-link to="/">
+              免费试用
+            </router-link>
+          </div>
+        </li>
+        <li>
+          <div class="product_ctn_tit">
+            <h3>体验版</h3>
+            <p>描述信息描述信息描述信息描述信息</p>
+          </div>
+          <div class="product_ctn_txt">
+            <p>
+              <span>容量</span>
+              <span>邮件无限容量</span>
+            </p>
+            <p>
+              <span>个人</span>
+              <span>个人网盘5G</span>
+            </p>
+            <p>
+              <span>共享</span>
+              <span>共享网盘15G</span>
+            </p>
+            <p>
+              <span>域名</span>
+              <span>多域名绑定</span>
+            </p>
+            <p>
+              <span>管理</span>
+              <span>多域统一管理</span>
+            </p>
+          </div>
+          <div class="product_ctn_money">
+            <p>
+              <span>0-20</span>
+              /用户
+            </p>
+          </div>
+          <div class="product_ctn_btn">
+            <router-link to="/">
+              免费试用
+            </router-link>
           </div>
         </li>
       </ul>
+    </div>
+  </div>
+
+  <!-- 价格计算 -->
+  <div class="money">
+    <div class="pub_title">
+      <h2>价格计算</h2>
+      <p>最新活动推荐，猜你喜欢</p>
+    </div>
+
+    <div class="money_box">
+      <div class="money_box_left">
+        <div class="money_box_left_tit">
+          9.4~9.10试用，享“买三年送三年”叠加优惠
+        </div>
+        <div class="money_box_left_bg">
+          <p>尊享版：<span>0</span></p>
+          <p>尊享版：<span>0</span></p>
+          <p>尊享版：<span>0</span></p>
+        </div>
+        <div class="money_box_left_mony">
+          <div class="money_box_left_mony_box">
+            <p>账户数</p>
+            <el-slider show-input size="small" />
+          </div>
+          <div class="money_box_left_mony_box">
+            <p>年限</p>
+            <el-slider show-input size="small" />
+          </div>
+        </div>
+      </div>
+      <div class="money_box_right">
+        <div class="money_box_right_tit">
+          <h3>优惠活动</h3>
+          <p>汇聚阿里云近100款爆款产品，云服务器低至1折起</p>
+        </div>
+        <div class="money_box_right_ctn">
+          <ul>
+            <li>618采购节限时折扣活动</li>
+            <li>中小型企业助力补贴</li>
+            <li>云钉一体企业数字化</li>
+          </ul>
+          <button class="money_box_right_inq">立即查看</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- 搭配推荐 -->
+  <div class="collo">
+    <div class="pub_title">
+      <h2>搭配产品推荐</h2>
+      <p>最新活动推荐，猜你喜欢</p>
     </div>
   </div>
 
@@ -110,5 +304,226 @@
   /* 产品规格 */
   .product {
     padding: 8vh 10%;
+  }
+
+  .product_ctn {
+    margin-top: 2rem;
+  }
+
+  .product_ctn ul {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .product_ctn ul li {
+    width: 22%;
+    margin-right: 3%;
+    box-sizing: border-box;
+    border: 1px solid #ededec;
+  }
+
+  .product_ctn ul li:hover {
+    box-shadow: 2px 2px 2px #E5E5E5, -1px -1px 1px #E5E5E5
+  }
+
+  .product_ctn_tit {
+    padding: 1rem;
+    box-sizing: border-box;
+    background-image: url(../assets/image/product_ctn_tit.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: right;
+  }
+
+  .product_ctn_tit h3 {
+    font-weight: 800;
+    line-height: 2.4rem;
+  }
+
+  .product_ctn_tit p {
+    font-size: .9rem;
+    width: 70%;
+    color: #999;
+  }
+
+  .product_ctn_txt {
+    padding: 1rem;
+    border-bottom: 1px solid #ededec;
+  }
+
+  .product_ctn_txt p {
+    display: flex;
+    width: 100%;
+    font-size: .8rem;
+    margin-bottom: 1rem;
+  }
+
+  .product_ctn_txt p span:nth-child(1) {
+    width: 30%;
+    color: #999;
+  }
+
+  .product_ctn_money {
+    padding: 0 1rem;
+    padding-top: .5rem;
+    text-align: center;
+    vertical-align: middle;
+  }
+
+  .product_ctn_money span {
+    font-weight: 800;
+    font-size: 1.4rem;
+    color: #ff6600;
+    margin-right: .2rem;
+  }
+
+  .product_ctn_money>p {
+    font-size: .8rem;
+  }
+
+  .product_ctn_btn {
+    text-align: center;
+    padding: 1rem 0;
+  }
+
+  .product_ctn_btn a {
+    color: #fff;
+    background-color: #ff6600;
+    padding: .2rem 2rem;
+    transition: .3s all;
+    font-size: .9rem;
+  }
+
+  .product_ctn_btn a:hover {
+    opacity: .8;
+  }
+
+  /* 价格计算 */
+  .money {
+    box-sizing: border-box;
+    overflow: hidden;
+    padding: 3rem 10%;
+  }
+
+  .money_box {
+    width: 100%;
+    box-sizing: border-box;
+    display: flex;
+    padding-top: 2rem;
+  }
+
+  .money_box_left {
+    width: 57%;
+    margin-right: 3%;
+  }
+
+  .money_box_left_tit {
+    padding: 2rem;
+    text-align: center;
+    color: #fff;
+    background-image: url(../assets/image/money_box_left_tit.png);
+    background-size: cover;
+  }
+
+  .money_box_left_bg {
+    margin: 0 1rem;
+    padding: 1rem 0;
+    display: flex;
+    border-bottom: 1px solid #E5E5E5;
+  }
+
+  .money_box_left_bg p {
+    text-align: center;
+    font-size: 1.1rem;
+    font-weight: 800;
+    width: 30%;
+    margin-right: 3%;
+  }
+
+  .money_box_left_bg span {
+    color: #ff6600;
+    font-weight: 800;
+  }
+
+  .money_box_left_mony {
+    padding: 1rem;
+  }
+
+  .money_box_left_mony_box {
+    margin-bottom: 2rem;
+  }
+
+  .money_box_left_mony_box p {
+    font-weight: 800;
+    font-size: .9rem;
+    margin-bottom: .5rem;
+  }
+
+  .money_box_right {
+    width: 38%;
+  }
+
+  .money_box_right_tit {
+    color: #fff;
+    padding: 1rem;
+    background-image: linear-gradient(129deg, #f34250 2%, #ff7d3b);
+  }
+
+  .money_box_right_tit h3 {
+    font-weight: 800;
+    line-height: 2rem;
+  }
+
+  .money_box_right_tit p {
+    font-size: .9rem;
+  }
+
+  .money_box_right_ctn {
+    padding: 3rem 10%;
+    background-image: url(../assets/image/money_box_right_ctn.png);
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+
+  .money_box_right_ctn ul {
+    padding-left: 0;
+  }
+
+  .money_box_right_ctn ul li {
+    margin-bottom: .6rem;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  .money_box_right_ctn ul li:hover {
+    color: #ff6600;
+  }
+
+  .money_box_right_ctn ul li::before {
+    display: block;
+    content: '';
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background-color: #ff6600;
+    margin-right: 1rem;
+  }
+
+  .money_box_right_inq {
+    padding: .5rem 2.4rem;
+    background-color: #ff6600;
+    color: #fff;
+    border: 0;
+    margin-top: 3rem;
+    transition: .3s all;
+  }
+
+  .money_box_right_inq:hover {
+    opacity: .8;
+  }
+
+  .collo {
+    padding: 3rem 10%;
   }
 </style>
